6bf3c1df96805d71697c2ea772289840126d78f6
Vasu <44114066+vasumsv@users.noreply.github.com>
2026-04-25 17:19:09 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -37,6 +45,7 @@ function App() {
         </div>
 
         <div className="relative z-10">
+          <ScrollToTop />
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
